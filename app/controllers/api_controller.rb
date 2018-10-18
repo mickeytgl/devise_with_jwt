43a4fe326476d091b72de0531376e183764ca341
Miguel Torres <migueltg93@gmail.com>
2018-10-18 17:51:06 -0500

    Added route

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,5 @@
 class ApiController < ApplicationController
+  skip_before_action :verify_authenticity_token
   attr_reader :current_user
   before_action :set_default_format
   before_action :authenticate_token!
